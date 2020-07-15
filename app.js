4704a87167f9676bfa47c6b973489304c549e2b3
David Breitenbuecher <dlbreitenbuecher@gmail.com>
2020-07-14 17:48:12 -0700

    trying to connect to giffy api

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+//First, we need to get the form data from the button
+//(select element with jQuery)
+//Create an event listener to run our functions
+//getGiffyAPI:
+    // get value of search term
+    // Use this to request info from Gify API
+        // eg.:
+            // let response = await response from axios.get('giffyAPIURL', {searchValue});
+    // console.log the response data
+
 console.log("Let's get this party started!");
+console.log($('#searchTerm').text());
+
+
+async function getGiff(){
+    let $searchTerm = $('#searchBar').val();
+    console.log($searchTerm);
+    let response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${$searchTerm}&api_key=r3OVinr80Qlphab877hyZg3MBHZmYCiZ&limit=1`);
+
+    console.log('Response from Giphy API', response);
+}
+
+$('#searchButton').on('submit', getGiff);
